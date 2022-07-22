60fcd431a0a26de35ec632ff26cac37c68abdd46
Manshilin Oleg <manshilin.o@gmail.com>
2022-07-23 00:37:03 +0300

    Solved more problems  Changes to be committed: 	modified:   five/dp_lib.py  Changes not staged for commit: 	modified:   .gitignore 	modified:   four/four_lib.py

--- five/dp_lib.py
+++ five/dp_lib.py
@@ -2,6 +2,8 @@ import numpy as np
 import numpy.typing as npt
 from numba import njit
 
+from consts.consts import PAM_MATRIX, BLOSUM_MATRIX
+
 
 def dp_change(money: int, coins: npt.NDArray[np.int64]) -> int:
     min_num_coins = np.zeros(money + 1, dtype=np.int64)
@@ -29,40 +31,197 @@ def manhattan_tourist(n: int, m: int, down, right) -> int:
 
 
 def lcs_backtrack(str_1, str_2, match_reward=1, mismatch_penalty=0, indel_penalty=0):
-    s = np.zeros((len(str_1), len(str_2)), dtype=np.int64)
-    backtrack = np.zeros((len(str_1), len(str_2)), dtype=np.int64)
-    for i in range(len(str_1)):
-        s[i, 0] = 0
-    for i in range(len(str_2)):
-        s[0, i] = 0
-    for i in range(1, len(str_1)):
-        for j in range(1, len(str_2)):
-            match = 1 if str_1[i] == str_2[j] else 0
-            s[i, j] = max(s[i - 1, j], s[i, j - 1], s[i - 1, j - 1] + match)
-            if s[i, j] == s[i - 1, j]:
-                backtrack[i, j] = 1
-            elif s[i, j] == s[i, j - 1]:
+    s = np.zeros((len(str_1) + 1, len(str_2) + 1), dtype=np.int64)
+    backtrack = np.zeros((len(str_1) + 1, len(str_2) + 1), dtype=np.int64)
+    for i in range(1, len(str_1) + 1):
+        s[i, 0] = s[i - 1, 0] - indel_penalty
+        backtrack[i, 0] = 2
+    for i in range(1, len(str_2) + 1):
+        s[0, i] = s[0, i - 1] - indel_penalty
+        backtrack[0, i] = 1
+    for i in range(1, len(str_1) + 1):
+        for j in range(1, len(str_2) + 1):
+            match = match_reward if str_1[i - 1] == str_2[j - 1] else -mismatch_penalty
+            s[i, j] = max(
+                s[i - 1, j] - indel_penalty,
+                s[i, j - 1] - indel_penalty,
+                s[i - 1, j - 1] + match,
+            )
+            if s[i, j] == s[i - 1, j] - indel_penalty:
                 backtrack[i, j] = 2
-            else:
+            elif s[i, j] == s[i, j - 1] - indel_penalty:
+                backtrack[i, j] = 1
+            elif match == match_reward:
                 backtrack[i, j] = 3
-    return backtrack
+            else:
+                backtrack[i, j] = 4
+    return backtrack, s[len(str_1), len(str_2)]
 
 
-def output_lcs(str1, str2):
-    backtrack = lcs_backtrack(str1, str2)
+def lcs_backtrack_local(str_1, str_2, score_matrix=PAM_MATRIX, indel_penalty=5):
+    s = np.zeros((len(str_1) + 1, len(str_2) + 1), dtype=np.int64)
+    backtrack = np.zeros((len(str_1) + 1, len(str_2) + 1), dtype=np.int64)
+    max_score = -np.inf
+    max_i, max_j = 0, 0
+    for i in range(1, len(str_1) + 1):
+        s[i, 0] = s[i - 1, 0] - indel_penalty
+        backtrack[i, 0] = 2
+    for i in range(1, len(str_2) + 1):
+        s[0, i] = s[0, i - 1] - indel_penalty
+        backtrack[0, i] = 1
+    for i in range(1, len(str_1) + 1):
+        for j in range(1, len(str_2) + 1):
+            # match = match_reward if str_1[i - 1] == str_2[j - 1] else -mismatch_penalty
+            match = score_matrix[str_1[i - 1]][str_2[j - 1]]
+            s[i, j] = max(
+                0,
+                s[i - 1, j] - indel_penalty,
+                s[i, j - 1] - indel_penalty,
+                s[i - 1, j - 1] + match,
+            )
+            if s[i,  j] > max_score:
+                max_score = s[i, j]
+                max_i, max_j = i, j
+            if s[i, j] == s[i - 1, j] - indel_penalty:
+                backtrack[i, j] = 2
+            elif s[i, j] == s[i, j - 1] - indel_penalty:
+                backtrack[i, j] = 1
+            elif (s[i, j] == s[i - 1, j - 1] + match) and str_1[i - 1] == str_2[j - 1]:
+                backtrack[i, j] = 3
+            elif (s[i, j] == s[i - 1, j - 1] + match) and str_1[i - 1] != str_2[j - 1]:
+                backtrack[i, j] = 4
+            else:
+                backtrack[i, j] = 0
+    return backtrack, s[max_i, max_j], max_i, max_j
+
+
+def lcs_backtrack_fitting(str_1, str_2, score_matrix=BLOSUM_MATRIX, indel_penalty=1):
+    s = np.zeros((len(str_1) + 1, len(str_2) + 1), dtype=np.int64)
+    backtrack = np.zeros((len(str_1) + 1, len(str_2) + 1), dtype=np.int64)
+    max_score = -np.inf
+    max_i, max_j = 0, 0
+    for i in range(1, len(str_1) + 1):
+        s[i, 0] = 0# s[i - 1, 0] - indel_penalty
+        backtrack[i, 0] = 0
+    for i in range(1, len(str_2) + 1):
+        s[0, i] = s[0, i - 1] - indel_penalty
+        backtrack[0, i] = 1
+    for i in range(1, len(str_1) + 1):
+        for j in range(1, len(str_2) + 1):
+            # match = match_reward if str_1[i - 1] == str_2[j - 1] else -mismatch_penalty
+            match = score_matrix[str_1[i - 1]][str_2[j - 1]]
+            s[i, j] = max(
+                s[i - 1, j] - indel_penalty,
+                s[i, j - 1] - indel_penalty,
+                s[i - 1, j - 1] + match,
+            )
+            if s[i,  j] > max_score:
+                max_score = s[i, j]
+                max_i, max_j = i, j
+            if s[i, j] == s[i - 1, j] - indel_penalty:
+                backtrack[i, j] = 2
+            elif s[i, j] == s[i, j - 1] - indel_penalty:
+                backtrack[i, j] = 1
+            elif (s[i, j] == s[i - 1, j - 1] + match) and str_1[i - 1] == str_2[j - 1]:
+                backtrack[i, j] = 3
+            elif (s[i, j] == s[i - 1, j - 1] + match) and str_1[i - 1] != str_2[j - 1]:
+                backtrack[i, j] = 4
+            else:
+                backtrack[i, j] = 0
+    print(s)
+    return backtrack, s[max_i, max_j], max_i, max_j
+
+
+def lcs_backtrack_overlap(str_1, str_2, match_reward=1,  mismatch_penalty=1, indel_penalty=1):
+    s = np.zeros((len(str_1) + 1, len(str_2) + 1), dtype=np.int64)
+    backtrack = np.zeros((len(str_1) + 1, len(str_2) + 1), dtype=np.int64)
+    max_score = -np.inf
+    max_i, max_j = 0, 0
+    for i in range(1, len(str_1) + 1):
+        s[i, 0] = 0# s[i - 1, 0] - indel_penalty
+        backtrack[i, 0] = 0
+    for i in range(1, len(str_2) + 1):
+        s[0, i] = 0 #s[0, i - 1] - indel_penalty
+        backtrack[0, i] = 0
+    for i in range(1, len(str_1) + 1):
+        for j in range(1, len(str_2) + 1):
+            match = match_reward if str_1[i - 1] == str_2[j - 1] else -mismatch_penalty
+            s[i, j] = max(
+                s[i - 1, j] - indel_penalty,
+                s[i, j - 1] - indel_penalty,
+                s[i - 1, j - 1] + match,
+            )
+            if s[i,  j] > max_score:
+                max_score = s[i, j]
+                max_i, max_j = i, j
+            if s[i, j] == s[i - 1, j] - indel_penalty:
+                backtrack[i, j] = 2
+            elif s[i, j] == s[i, j - 1] - indel_penalty:
+                backtrack[i, j] = 1
+            elif (s[i, j] == s[i - 1, j - 1] + match) and str_1[i - 1] == str_2[j - 1]:
+                backtrack[i, j] = 3
+            elif (s[i, j] == s[i - 1, j - 1] + match) and str_1[i - 1] != str_2[j - 1]:
+                backtrack[i, j] = 4
+            else:
+                backtrack[i, j] = 0
+    print(s)
+    return backtrack, s[max_i, max_j], max_i, max_j
+
+
+
+def output_lcs(str1, str2, match_reward=1, mismatch_penalty=0, indel_penalty=0):
+    #backtrack, score = lcs_backtrack(
+    #   str1, str2, match_reward, mismatch_penalty, indel_penalty
+    #)
+    backtrack, score, max_i, max_j = lcs_backtrack_overlap(str1, str2, match_reward, mismatch_penalty, indel_penalty)
     print(backtrack)
 
-    def __output_lcs(backtrack, str_1, i, j):
-        if i == -1 or j == -1:
+    def __output_lcs0(backtrack, str_1, i, j):
+        if i == 0 and j == 0:
             return ""
-        if backtrack[i, j] == 1:
-            return __output_lcs(backtrack, str_1, i - 1, j)
+        if backtrack[i, j] == 0:
+            return ""
+        elif backtrack[i, j] == 1:
+            return __output_lcs0(backtrack, str_1, i, j - 1)
         elif backtrack[i, j] == 2:
-            return __output_lcs(backtrack, str_1, i, j - 1)
+            return __output_lcs0(backtrack, str_1, i - 1, j)
+        elif backtrack[i, j] == 3:
+            return __output_lcs0(backtrack, str_1, i - 1, j - 1) + str_1[i - 1]
         else:
-            return __output_lcs(backtrack, str_1, i - 1, j - 1) + str_1[i]
+            return __output_lcs0(backtrack, str_1, i - 1, j - 1)
 
-    return __output_lcs(backtrack, str1, len(str1) - 1, len(str2) - 1)
+    def __output_lcs1(backtrack, str_1, i, j):
+        if i == 0 and j == 0:
+            return ""
+        if backtrack[i, j] == 0:
+            return ""
+        elif backtrack[i, j] == 1:
+            return __output_lcs1(backtrack, str_1, i, j - 1) + "-"
+        elif backtrack[i, j] == 2:
+            return __output_lcs1(backtrack, str_1, i - 1, j) + str_1[i - 1]
+        else:
+            return __output_lcs1(backtrack, str_1, i - 1, j - 1) + str_1[i - 1]
+
+    def __output_lcs2(backtrack, str_1, i, j):
+        if j == 0 and i == 0:
+            return ""
+        if backtrack[i, j] == 0:
+            return ""
+        elif backtrack[i, j] == 1:
+            return __output_lcs2(backtrack, str_1, i, j - 1) + str_1[j - 1]
+        elif backtrack[i, j] == 2:
+            return __output_lcs2(backtrack, str_1, i - 1, j) + "-"
+        else:
+            return __output_lcs2(backtrack, str_1, i - 1, j - 1) + str_1[j - 1]
+
+    return (
+        score,
+        __output_lcs1(backtrack, str1, max_i, max_j),
+        __output_lcs2(backtrack, str2, max_i, max_j),
+        #__output_lcs1(backtrack, str1, len(str1), len(str2)),
+        #__output_lcs2(backtrack, str2, len(str1), len(str2)),
+        #__output_lcs0(backtrack, str2, len(str1), len(str2)),
+    )
 
 
 class Grph_path:
