b4fc7b06f25e2048787563afd42034e920ff57bd
Manshilin Oleg <manshilin.o@gmail.com>
2022-07-30 16:09:37 +0300

    Finished 3 string aligment task. Completed part 5 of bioinformatics course  On branch main  Changes to be committed: 	modified:   five/dp_lib.py

--- five/dp_lib.py
+++ five/dp_lib.py
@@ -18,7 +18,7 @@ def dp_change(money: int, coins: npt.NDArray[np.int64]) -> int:
     return min_num_coins[money]
 
 
-#@njit()
+# @njit()
 def manhattan_tourist(n: int, m: int, down, right) -> int:
     s = np.zeros((n, m), dtype=np.int64)
     for i in range(1, n):
@@ -58,49 +58,149 @@ def lcs_backtrack(str_1, str_2, match_reward=1, mismatch_penalty=0, indel_penalt
     return backtrack, s[len(str_1), len(str_2)]
 
 
-def lcs_backtrack_3d(str_1, str_2, str_3, match_reward=1, mismatch_penalty=0, indel_penalty=0):
+def lcs_backtrack_3d(
+    str_1, str_2, str_3, match_reward=1, mismatch_penalty=0, indel_penalty=0
+):
     s = np.zeros((len(str_1) + 1, len(str_2) + 1, len(str_3) + 1), dtype=np.int64)
-    backtrack = np.zeros((len(str_1) + 1, len(str_2) + 1, len(str_3) + 1), dtype=np.int64)
+    backtrack = np.zeros(
+        (len(str_1) + 1, len(str_2) + 1, len(str_3) + 1), dtype=np.int64
+    )
     for i in range(1, len(str_1) + 1):
         s[i, 0, 0] = s[i - 1, 0, 0] - indel_penalty
-        backtrack[i, 0, 0] = 2
+        backtrack[i, 0, 0] = 1
     for j in range(1, len(str_2) + 1):
         s[0, j, 0] = s[0, j - 1, 0] - indel_penalty
-        backtrack[0, j, 0] = 1
+        backtrack[0, j, 0] = 2
     for k in range(1, len(str_3) + 1):
         s[0, 0, k] = s[0, 0, k - 1] - indel_penalty
-        backtrack[0, 0, k] = 1
+        backtrack[0, 0, k] = 3
+
+    for i in range(1, len(str_1) + 1):
+        for j in range(1, len(str_2) + 1):
+            s[i, j, 0] = s[i - 1, j - 1, 0] - indel_penalty
+            backtrack[i, j, 0] = 4
+    for i in range(1, len(str_1) + 1):
+        for k in range(1, len(str_3) + 1):
+            s[i, 0, k] = s[i - 1, 0, k - 1] - indel_penalty
+            backtrack[i, 0, k] = 5
+    for j in range(1, len(str_2) + 1):
+        for k in range(1, len(str_3) + 1):
+            s[0, j, k] = s[0, j - 1, k - 1] - indel_penalty
+            backtrack[0, j, k] = 6
+
     for i in range(1, len(str_1) + 1):
         for j in range(1, len(str_2) + 1):
             for k in range(1, len(str_3) + 1):
-                #match1 = match_reward if str_1[i - 1] == str_2[j - 1] else -mismatch_penalty
-                #match2 = match_reward if str_1[i - 1] == str_3[j - 1] else -mismatch_penalty
-                #match3 = match_reward if str_2[i - 1] == str_3[j - 1] else -mismatch_penalty
+                match = (
+                    match_reward
+                    if str_1[i - 1] == str_2[j - 1] and str_2[j - 1] == str_3[k - 1]
+                    else -mismatch_penalty
+                )
 
-                match4 = match_reward if str_1[i - 1] == str_2[j - 1] and \
-                         str_2[j - 1] == str_3[k - 1] else -mismatch_penalty
-
-                s[i, j] = max(
+                s[i, j, k] = max(
                     s[i - 1, j, k] - indel_penalty,
                     s[i, j - 1, k] - indel_penalty,
                     s[i, j, k - 1] - indel_penalty,
                     s[i - 1, j - 1, k] - indel_penalty,
                     s[i - 1, j, k - 1] - indel_penalty,
                     s[i, j - 1, k - 1] - indel_penalty,
-                    s[i - 1, j - 1, k - 1] + match4
+                    s[i - 1, j - 1, k - 1] + match,
                 )
-                """
-                if s[i, j] == s[i - 1, j] - indel_penalty:
-                    backtrack[i, j] = 2
-                elif s[i, j] == s[i, j - 1] - indel_penalty:
-                    backtrack[i, j] = 1
-                elif match == match_reward:
-                    backtrack[i, j] = 3
+                if s[i, j, k] == s[i - 1, j, k] - indel_penalty:
+                    backtrack[i, j, k] = 1
+                elif s[i, j, k] == s[i, j - 1, k] - indel_penalty:
+                    backtrack[i, j, k] = 2
+                elif s[i, j, k] == s[i, j, k - 1] - indel_penalty:
+                    backtrack[i, j, k] = 3
+                elif s[i, j, k] == s[i - 1, j - 1, k] - indel_penalty:
+                    backtrack[i, j, k] = 4
+                elif s[i, j, k] == s[i - 1, j, k - 1] - indel_penalty:
+                    backtrack[i, j, k] = 5
+                elif s[i, j, k] == s[i, j - 1, k - 1] - indel_penalty:
+                    backtrack[i, j, k] = 6
                 else:
-                    backtrack[i, j] = 4
-                """
-    return s[len(str_1), len(str_2), len(str_3)] #backtrack, s[len(str_1), len(str_2)]
+                    backtrack[i, j, k] = 7
+    # print(backtrack)
+    # print(s)
+    return (
+        s[len(str_1), len(str_2), len(str_3)],
+        backtrack,
+    )
 
+
+def output_lcs_3d(
+    str_1, str_2, str_3, match_reward=1, mismatch_penalty=0, indel_penalty=0
+):
+    score, backtrack = lcs_backtrack_3d(
+        str_1, str_2, str_3, match_reward, mismatch_penalty, indel_penalty
+    )
+
+    def __output_lcs1(backtrack, str_1, i, j, k):
+        if i == 0 and j == 0 and k == 0:
+            return ""
+        if backtrack[i, j, k] == 0:
+            return ""
+        elif backtrack[i, j, k] == 1:
+            return __output_lcs1(backtrack, str_1, i - 1, j, k) + str_1[i - 1]
+        elif backtrack[i, j, k] == 2:
+            return __output_lcs1(backtrack, str_1, i, j - 1, k) + "-"
+        elif backtrack[i, j, k] == 3:
+            return __output_lcs1(backtrack, str_1, i, j, k - 1) + "-"
+        elif backtrack[i, j, k] == 4:
+            return __output_lcs1(backtrack, str_1, i - 1, j - 1, k) + str_1[i - 1]
+        elif backtrack[i, j, k] == 5:
+            return __output_lcs1(backtrack, str_1, i - 1, j, k - 1) + str_1[i - 1]
+        elif backtrack[i, j, k] == 6:
+            return __output_lcs1(backtrack, str_1, i, j - 1, k - 1) + "-"
+        else:
+            return __output_lcs1(backtrack, str_1, i - 1, j - 1, k - 1) + str_1[i - 1]
+
+    def __output_lcs2(backtrack, str_2, i, j, k):
+        if i == 0 and j == 0 and k == 0:
+            return ""
+        if backtrack[i, j, k] == 0:
+            return ""
+        elif backtrack[i, j, k] == 1:
+            return __output_lcs2(backtrack, str_2, i - 1, j, k) + "-"
+        elif backtrack[i, j, k] == 2:
+            return __output_lcs2(backtrack, str_2, i, j - 1, k) + str_2[j - 1]
+        elif backtrack[i, j, k] == 3:
+            return __output_lcs2(backtrack, str_2, i, j, k - 1) + "-"
+        elif backtrack[i, j, k] == 4:
+            return __output_lcs2(backtrack, str_2, i - 1, j - 1, k) + str_2[j - 1]
+        elif backtrack[i, j, k] == 5:
+            return __output_lcs2(backtrack, str_2, i - 1, j, k - 1) + "-"
+        elif backtrack[i, j, k] == 6:
+            return __output_lcs2(backtrack, str_2, i, j - 1, k - 1) + str_2[j - 1]
+        else:
+            return __output_lcs2(backtrack, str_2, i - 1, j - 1, k - 1) + str_2[j - 1]
+
+    def __output_lcs3(backtrack, str_3, i, j, k):
+        if i == 0 and j == 0 and k == 0:
+            return ""
+        if backtrack[i, j, k] == 0:
+            return ""
+        elif backtrack[i, j, k] == 1:
+            return __output_lcs3(backtrack, str_3, i - 1, j, k) + "-"
+        elif backtrack[i, j, k] == 2:
+            return __output_lcs3(backtrack, str_3, i, j - 1, k) + "-"
+        elif backtrack[i, j, k] == 3:
+            return __output_lcs3(backtrack, str_3, i, j, k - 1) + str_3[k - 1]
+        elif backtrack[i, j, k] == 4:
+            return __output_lcs3(backtrack, str_3, i - 1, j - 1, k) + "-"
+        elif backtrack[i, j, k] == 5:
+            return __output_lcs3(backtrack, str_3, i - 1, j, k - 1) + str_3[k - 1]
+        elif backtrack[i, j, k] == 6:
+            return __output_lcs3(backtrack, str_3, i, j - 1, k - 1) + str_3[k - 1]
+        else:
+            return __output_lcs3(backtrack, str_3, i - 1, j - 1, k - 1) + str_3[k - 1]
+
+    return (
+        score,
+        __output_lcs1(backtrack, str_1, len(str_1), len(str_2), len(str_3)),
+        __output_lcs2(backtrack, str_2, len(str_1), len(str_2), len(str_3)),
+        __output_lcs3(backtrack, str_3, len(str_1), len(str_2), len(str_3)),
+    )
 
 
 @njit()
@@ -115,7 +215,7 @@ def middle_node(str_1, str_2, match_reward=1, mismatch_penalty=0, indel_penalty=
         score = np.zeros((len(str_1) + 1, 2), dtype=np.int64)
         for i in range(1, len(str_1) + 1):
             score[i, 0] = score[i - 1, 0] - indel_penalty
-        #score[0, 1] = score[0, 0] - indel_penalty
+        # score[0, 1] = score[0, 0] - indel_penalty
 
         for j in range(1, len(str_2) + 1):
             score[0, j % 2] = score[0, (j + 1) % 2] - indel_penalty
@@ -148,22 +248,7 @@ def middle_node(str_1, str_2, match_reward=1, mismatch_penalty=0, indel_penalty=
     edge1 = (np.argmax(so_m[:, -1] + m_si[:, 0]), middle)
     f_node_i, f_node_j = edge1
 
-    #print((str_1))
-    #print((str_2))
-    #print(len(str_1))
-    #print(len(str_2))
-    #print(f_node_i)
-    #print(m_si)
-    #print(f_node_i)
-    #print(m_si)
     sz1, _ = m_si.shape
-    #print(sz1)
-    #print(f_node_i)
-    #if f_node_i == sz1 - 1:
-    ##    s_node_i = f_node_i
-    #    s_node_j = f_node_j + 1
-    #    edge_dir = 1
-    #    return (int(f_node_i), f_node_j, int(s_node_i), s_node_j), edge_dir
     if f_node_i < sz1 - 1:
         if m_si[f_node_i, 0] + indel_penalty == m_si[f_node_i, 1]:
             s_node_i = f_node_i
@@ -178,9 +263,9 @@ def middle_node(str_1, str_2, match_reward=1, mismatch_penalty=0, indel_penalty=
             s_node_j = f_node_j + 1
             edge_dir = 3
     else:
-            s_node_i = f_node_i
-            s_node_j = f_node_j + 1
-            edge_dir = 1
+        s_node_i = f_node_i
+        s_node_j = f_node_j + 1
+        edge_dir = 1
     return (int(f_node_i), f_node_j, int(s_node_i), s_node_j), edge_dir
 
 
@@ -207,10 +292,14 @@ def linear_space_aligment(
     middle = int(np.floor((right + left) / 2))
     print("Yay1")
     middle_edge_coord, edge_direction = middle_node(
-        str_1[top:bottom], str_2[left:right], math_reward, mismatch_penalty, indel_penalty
+        str_1[top:bottom],
+        str_2[left:right],
+        math_reward,
+        mismatch_penalty,
+        indel_penalty,
     )
     mid_node, _, _, _ = middle_edge_coord
-    mid_node += top#left
+    mid_node += top  # left
     print("Yay2")
     linear_space_aligment(
         str_1,
@@ -226,7 +315,7 @@ def linear_space_aligment(
     )
     path.append(edge_direction)
     if edge_direction == 6:
-        path += [1]*(right - middle)
+        path += [1] * (right - middle)
         return
     if edge_direction == 1 or edge_direction == 3:
         middle += 1
@@ -247,7 +336,14 @@ def linear_space_aligment(
     return
 
 
-def find_path_back(dna_1: str, dna_2: str, path: list[int], match_reward=1, mismatch_penalty=0, indel_penalty=0):
+def find_path_back(
+    dna_1: str,
+    dna_2: str,
+    path: list[int],
+    match_reward=1,
+    mismatch_penalty=0,
+    indel_penalty=0,
+):
     i = len(dna_1) - 1
     j = len(dna_2) - 1
     aligment_1: str = ""
@@ -256,25 +352,23 @@ def find_path_back(dna_1: str, dna_2: str, path: list[int], match_reward=1, mism
     for edge_dir in reversed(path):
         if edge_dir == 3:
             score += match_reward if dna_1[i] == dna_2[j] else -mismatch_penalty
-            aligment_1 += dna_1[i] 
+            aligment_1 += dna_1[i]
             aligment_2 += dna_2[j]
             i -= 1
             j -= 1
         if edge_dir == 2:
             score -= indel_penalty
-            aligment_1 += dna_1[i] 
-            aligment_2 += "-"#dna_2[j]
+            aligment_1 += dna_1[i]
+            aligment_2 += "-"  # dna_2[j]
             i -= 1
-            #j -= 1
+            # j -= 1
         if edge_dir == 1:
             score -= indel_penalty
-            aligment_1 += "-"#dna_1[i] 
+            aligment_1 += "-"  # dna_1[i]
             aligment_2 += dna_2[j]
-            #i -= 1
+            # i -= 1
             j -= 1
     return score, aligment_1[::-1], aligment_2[::-1]
-
-
 
 
 def lcs_backtrack_local(str_1, str_2, score_matrix=PAM_MATRIX, indel_penalty=5):
